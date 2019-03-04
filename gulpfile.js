daea1490b97312ca0f03349fe9d40766c2dc3fab
Your Name <you@example.com>
2019-03-04 18:21:04 +0200

    mobile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@ var gulp = require('gulp'),
 
 
 gulp.task('sass', function(){
-    return gulp.src('./sass/**/*style.{sass,scss}')
+    return gulp.src('./sass/**/*styledesc.{sass,scss}')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+});
+gulp.task('sassmob', function(){
+    return gulp.src('./sass/**/*stylemob.{sass,scss}')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+});
+gulp.task('sasspl', function(){
+    return gulp.src('./sass/**/*stylepl.{sass,scss}')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 });
 
+
 gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.{sass,scss}' , gulp.parallel('sass'));
+    gulp.watch('./sass/**/*.{sass,scss}' , gulp.parallel('sass','sassmob','sasspl'));
 });
 
 gulp.task('default', gulp.parallel('watch'));
