61353ee7aebf6d0e2724457569e2a68a244c1498
Your Name <you@example.com>
2019-03-11 09:55:38 +0200

    all

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,17 @@ var gulp = require('gulp'),
 
 
 gulp.task('sass', function(){
-    return gulp.src('./sass/**/*styledesc.{sass,scss}')
+    return gulp.src('./sass/**/*main_1920.{sass,scss}')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 });
 gulp.task('sassmob', function(){
-    return gulp.src('./sass/**/*stylemob.{sass,scss}')
+    return gulp.src('./sass/**/*main_320.{sass,scss}')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 });
 gulp.task('sasspl', function(){
-    return gulp.src('./sass/**/*stylepl.{sass,scss}')
+    return gulp.src('./sass/**/*main_767.{sass,scss}')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 });
